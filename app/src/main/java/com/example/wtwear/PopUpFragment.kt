a7723774c8924fa89689f045223d993a36665deb
Alfie <alfieridwan2005@gmail.com>
2024-01-08 04:50:55 +0000

    removed topbar (use Theme.WTWear)

--- app/src/main/java/com/example/wtwear/PopUpFragment.kt
+++ app/src/main/java/com/example/wtwear/PopUpFragment.kt
@@ -1,14 +1,12 @@
 package com.example.wtwear
 
 import android.os.Bundle
-import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import android.widget.ImageView
 import android.widget.TextView
 import androidx.fragment.app.DialogFragment
-import androidx.lifecycle.Observer
 
 
 class PopUpFragment : DialogFragment() {
